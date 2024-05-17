a2dabb13fbb1b3aa0d6a342cf368ad91003cc4db
Carlos Loeza <cloeza1@mail.ccsf.edu>
2024-05-17 13:31:22 -0700

    Initial Commit

---
+++ Login-FastAPI/Login_FastAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_FastAPIApp.swift
+//  Login-FastAPI
+//
+//  Created by Carlos on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Login_FastAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
